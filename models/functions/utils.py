24ba94236f8f9a2fbee9b4c098c06443308adbed
khuynh <hkennyv@gmail.com>
2021-01-30 10:16:45 -0800

    fix: update function to be a single-model function to work around cloud function storage limitations

--- models/functions/utils.py
+++ models/functions/utils.py
@@ -1,34 +1,25 @@
+import os
+
 from typing import Optional
 from pathlib import Path
 
 from aitextgen import aitextgen
+from google.cloud import storage
 
-DIR_MODELS = Path("trained_models")
 
 # cache models for serverless function
-models = {
-    "elias": aitextgen(
-        model=DIR_MODELS / "elias/v2/pytorch_model.bin",
-        config=DIR_MODELS / "elias/v2/config.json",
-    ),
-    "jai": aitextgen(
-        model=DIR_MODELS / "jai/v1/pytorch_model.bin",
-        config=DIR_MODELS / "jai/v1/config.json",
-    ),
-    "diana": aitextgen(
-        model=DIR_MODELS / "diana/v1/pytorch_model.bin",
-        config=DIR_MODELS / "diana/v1/config.json",
-    ),
-    "saumil": aitextgen(
-        model=DIR_MODELS / "saumil/v1/pytorch_model.bin",
-        config=DIR_MODELS / "saumil/v1/config.json",
-    ),
-}
+client = storage.Client("gpt2-models")
+bucket = client.get_bucket("gpt2-models")
 
+# download 
+pytorch_model = bucket.get_blob(f"trained_models/{os.environ.get('model_name')}/v1/pytorch_model.bin")
+pytorch_model.download_to_filename("/tmp/model.bin")
+
+config = bucket.get_blob(f"trained_models/{os.environ.get('model_name')}/v1/config.json")
+config.download_to_filename("/tmp/config.json")
 
 def generate_text_from_model(
     n: int,
-    model: str,
     prompt: str,
     min_length: int = None,
     max_length: int = 256,
@@ -36,10 +27,7 @@ def generate_text_from_model(
     top_p: float = 0.7,
     **kwargs
 ) -> Optional[str]:
-    ai = models.get(model, None)
-
-    if ai is None:
-        return None
+    ai = aitextgen(model="/tmp/model.bin", config="/tmp/config.json")
 
     res = ai.generate(
         n=n,
