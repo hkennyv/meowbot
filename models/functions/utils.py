c5a0d3c2a21cc0ef1bc5f266e15e3a8ca6f1fb02
khuynh <hkennyv@gmail.com>
2021-05-24 20:47:10 -0500

    bump requirements and update scripts

--- models/functions/utils.py
+++ models/functions/utils.py
@@ -16,14 +16,14 @@ logger.setLevel(logging.DEBUG)
 # cache model to lower response times
 TMP_PATH = Path(os.environ.get("TMP_DIR", "./"))
 
-if not (TMP_PATH / "model.bin").exists():
+if not (TMP_PATH / "pytorch_model.bin").exists():
     client = storage.Client("gpt2-models")
     bucket = client.get_bucket("gpt2-models")
 
     pytorch_model = bucket.get_blob(
         f"trained_models/{os.environ.get('model_name')}/v1/pytorch_model.bin"
     )
-    pytorch_model.download_to_filename(TMP_PATH / "model.bin")
+    pytorch_model.download_to_filename(TMP_PATH / "pytorch_model.bin")
 
     config = bucket.get_blob(
         f"trained_models/{os.environ.get('model_name')}/v1/config.json"
@@ -33,8 +33,8 @@ if not (TMP_PATH / "model.bin").exists():
 logger.debug(f"{torch.cuda.is_available()=}")
 
 ai = aitextgen(
-    model=TMP_PATH / "model.bin",
-    config=TMP_PATH / "config.json",
+    model_folder=TMP_PATH,
+    config=TMP_PATH,
     to_gpu=torch.cuda.is_available(),
 )
 
--- models/scripts/gentext.py
+++ models/scripts/gentext.py
@@ -6,28 +6,34 @@ some text from it. it heavily uses the aitextgen framework:
 
 https://github.com/minimaxir/aitextgen
 """
-from aitextgen import aitextgen
+import configparser
 from pathlib import Path
 
+from aitextgen import aitextgen
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
 # user configurable
-TRAINED_MODELS = Path("trained_models").resolve()
-MODEL_NAME = "elias"
-MODEL_VERSION = "v2"
+TRAINED_MODELS = config["DEFAULT"]["TRAINED_MODEL_FOLDER"]
+MODEL_NAME = config["DEFAULT"]["MODEL_NAME"]
+MODEL_VERSION = config["DEFAULT"]["MODEL_VERSION"]
 
 # construct the directory that contains the model and config
-directory = TRAINED_MODELS / MODEL_NAME / MODEL_VERSION
+directory = Path(TRAINED_MODELS).resolve() / MODEL_NAME / MODEL_VERSION
 
 
-ai = aitextgen(model=directory / "pytorch_model.bin", config=directory / "config.json")
-# ai.quantize()
+ai = aitextgen(model_folder=directory, config=directory)
 
-ai.generate(
-    n=3,
-    batch_size=5,
-    prompt="how are you doing?",
-    min_length=25,
-    max_length=256,
-    temperature=2.3,
-    top_p=0.9,
-    seed=0,
-)
+# ai.generate(
+#     n=3,
+#     batch_size=5,
+#     # prompt="i would give anything to",
+#     min_length=256,
+#     max_length=1024,
+#     temperature=2.3,
+#     top_p=0.9,
+#     seed=0,
+# )
+
+ai.generate(n=10)
